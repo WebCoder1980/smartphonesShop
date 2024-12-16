bb287c1c38c65c57b0b848dde4f8478b4adf213b
Smagin Maxim Alexeevich <maxprogger@mail.ru>
2024-12-16 16:12:53 +0300

    Deleted interface IdbService.cs

--- Repository/DbService.cs
+++ Repository/DbService.cs
@@ -9,7 +9,7 @@ using System.Windows;
 
 namespace SmartphoneShop.Repository
 {
-    public class DbService : IDbService
+    public class DbService
     {
         public List<ProductModel> getAllProducts()
         {
