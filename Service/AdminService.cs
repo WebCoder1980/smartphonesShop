3fcda3c6fdcd5b72c8390a7cd4ada38c088c28a4
Smagin Maxim Alexeevich <maxprogger@mail.ru>
2024-12-17 15:27:49 +0300

    Created AdminService

---
+++ Service/AdminService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartphoneShop.Service
+{
+    internal class AdminService
+    {
+        SesseionInfo CurrentSessionInfo { get; set; }
+        public AdminService(SesseionInfo CurrentSessionInfo) {
+            this.CurrentSessionInfo = CurrentSessionInfo;
+        }
+
+
+    }
+}
